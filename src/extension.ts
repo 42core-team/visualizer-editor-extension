f5b95c90a684836d70c2b197676873a6d9ad7418
Reptudn <jonas.kauker2003@gmail.com>
2025-05-23 15:10:51 +0200

    display visualizer in iframe but its not in full width and height for some reason

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,10 @@ export function activate(context: vscode.ExtensionContext) {
     const panel = vscode.window.createWebviewPanel(
       'visualizer', // Identifies the type of the webview. Used internally
       'CORE GAME Visualizer',       // Title of the panel displayed to the user
-      vscode.ViewColumn.One, // Editor column to show the new webview panel in
-      {} // Webview options. More on these later.
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+      }
     );
 
     // Set the webview's HTML content
@@ -18,17 +20,37 @@ export function activate(context: vscode.ExtensionContext) {
 
 function getWebviewContent() {
   return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Hello World</title>
-    </head>
-    <body>
-      <h1>Hello World</h1>
-    </body>
-    </html>
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>CORE GAME Visualizer</title>
+  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; frame-src http://localhost;">
+  <style>
+    html, body {
+      height: 100%;
+      width: 100%;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+    }
+  </style>
+</head>
+<body>
+  <iframe src="http://localhost/" style="
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: none;
+    width: 100vw;
+    height: 100vh;
+    transform: scale(2); 
+    transform-origin: top left;
+  "></iframe>
+</body>
+</html>
   `;
 }
+
 
 export function deactivate() {}
